e90d9e65e4e13d5281063f5909eabf784e4050fb
Arkkarachat  Siribout <arkkarachat.s@gmail.com>
2020-07-19 21:27:50 +0700

    first commit

---
+++ application/controllers/Customer.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Customer extends CI_Controller {
+
+	public function index()
+	{
+        echo 'Hello Customer';
+		// $this->load->view('welcome_message');
+	}
+}
